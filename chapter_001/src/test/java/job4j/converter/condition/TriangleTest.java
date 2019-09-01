eb1d6a6f4675a69c65008486b4c33bbad4999950
slash25 <jamaslash@gmail.com>
2019-09-01 12:30:14 +0300

    ExistsTriangle with Tests [169773]

---
+++ chapter_001/src/test/java/job4j/converter/condition/TriangleTest.java
@@ -0,0 +1,16 @@
+package job4j.converter.condition;
+import org.junit.Ignore;
+import org.junit.Test;
+import ru.job4j.condition.Triangle;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+    @Ignore
+    public class TriangleTest {
+        @Test
+        public void whenExist() {
+            boolean result = Triangle.exist(2.0, 2.0, 2.0);
+            assertThat(result, is(true));
+        }
+}
