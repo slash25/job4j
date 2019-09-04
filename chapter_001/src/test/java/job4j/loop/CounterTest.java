cf9c409d9c3008cad6e411031e71070029b79ff4
slash25 <jamaslash@gmail.com>
2019-09-04 09:33:21 +0300

    Counter amount of even numbers with Tests [169778]

---
+++ chapter_001/src/test/java/job4j/loop/CounterTest.java
@@ -0,0 +1,17 @@
+package job4j.loop;
+import org.junit.Test;
+import ru.job4j.loop.Counter;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове
+        Counter prov = new Counter();
+        int result = prov.add(1,10);
+        assertThat(result,is(30));
+    }
+
+}
