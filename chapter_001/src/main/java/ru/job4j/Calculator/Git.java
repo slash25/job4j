d5962bed358524b2d2776252b29991c58a9b7b59
slash25 <jamaslash@gmail.com>
2019-09-08 20:52:29 +0300

    Calculate with JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculator/Git.java
+++
@@ -1,4 +0,0 @@
-package ru.job4j.Calculator;
-
-public class Git {
-}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,42 @@
+package ru.job4j.calculate;
+
+/** Class for calculate  arifmetic operation add, division, multiply, subtruck
+ *@author Terekhov Bronislav
+ * @since 8.09.219
+ * @version 1
+ */
+
+public class Calculate {
+    /**
+     *
+     * @param args different arguments for fours metods  "+"; "/"; "*"; "-"
+     */
+    public static void main(String[] args) {
+        add(1, 1);
+        div(4,2);
+        multiply(2,1);
+        subtruck(10,5);
+    }
+
+
+    private static void add(int first, int second) {
+        int result =  first + second;
+        System.out.println(first + " + "  + second + " = " + result);
+    }
+
+
+    private static  void div(int first, int second){
+        int result = first / second;
+        System.out.println(first + " / " + second + " = " + result);
+    }
+
+    private static  void multiply(int first, int second){
+        int result = first * second;
+        System.out.println(first + " * " + second + " = " + result);
+    }
+
+    private static  void subtruck(int first, int second){
+        int result = first - second;
+        System.out.println(first + " * " + second + " = " + result);
+    }
+}
