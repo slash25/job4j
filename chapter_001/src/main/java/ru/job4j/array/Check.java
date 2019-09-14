377658c478ed30d9c9b654a6451d1b69979222f3
slash25 <jamaslash@gmail.com>
2019-09-14 22:17:09 +0300

    Check with Test

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Check {
+    public boolean mono(boolean[] data) {
+        int countTue = 0;
+        int counFalse = 0;
+        boolean result = false;
+        for (boolean value :  data) {
+            if (value){//if value == true
+                countTue++;
+            }else
+            counFalse++;
+        }
+
+        if (countTue == data.length | counFalse == data.length) {
+            result = true;
+        }
+        return result;
+    }
+}
