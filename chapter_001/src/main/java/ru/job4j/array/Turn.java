8188019fb63e0caa9fbcc2f681f1a01f17281bd1
slash25 <jamaslash@gmail.com>
2019-09-14 10:33:02 +0300

    Turn Array With Test

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -5,23 +5,10 @@ public class Turn {
 
         for (int index = 0; index < array.length; index++) {
             int k = array.length - index - 1;
-
-            if (array.length % 2 != 0){
-                if(array[k] != array[index]){
-                   int temp = array[index];
-                   array[index] = array[k];
-                   array[k] = temp;
-
-                }
-                else
-                return array;
-            }
-            else {
-                if (k >  index){
-                    int temp = array[index];
-                    array[index] = array[k];
-                    array[k] = temp;
-                }
+            if (index < array.length / 2) {
+                int temp = array[index];
+                array[index] = array[k];
+                array[k] = temp;
             }
         }
         return array;
--- chapter_001/src/test/java/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/job4j/array/TurnTest.java
@@ -17,11 +17,20 @@ public class TurnTest {
     }
 
     @Test
-    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {//
         Turn turner = new Turn();
         int[] input = new int[] {1,2,3,4,5,};
         int[] result = turner.back(input);
         int[] expect = new int[] {5,4,3,2,1};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWith123456789() {//
+        Turn turner = new Turn();
+        int[] input = new int[] {1,2,3,4,5,6,7,8,9};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {9,8,7,6,5,4,3,2,1};
         assertThat(result, is(expect));
     }
 }
