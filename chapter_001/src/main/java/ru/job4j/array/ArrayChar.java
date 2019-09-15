6adb159610d20b417736943faa8fa7ceb84ae889
slash25 <jamaslash@gmail.com>
2019-09-15 22:48:33 +0300

    ArrayCharStarts with Test

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,11 +4,8 @@ public class ArrayChar {
     public static boolean startsW(char[] word, char[] pref) {
         boolean result = false;
         for (int  index = 0; index < pref.length; index++){
-            if (pref[index] == word[index] ){
-                result = true;
-            }
-            else
-            result = false;
+            //if (pref[index] == word[index] )  result = true;
+            result = pref[index] == word[index];
         }
         return result;
     }
