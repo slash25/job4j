69d7b796a38b2c8660f91f745c497e7c83186c24
slash25 <jamaslash@gmail.com>
2019-09-16 05:42:45 +0300

    ArrayCharEnds with Test

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+//проверить. что массив word имеет последние элементы одинаковые с post*/
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        int counter = 0;
+        int postIndex = 0;
+
+            for (int index = word.length - post.length; index < word.length; index++) {
+                if (post[postIndex] == word[index]) {
+                    counter++;
+                }
+            postIndex ++;
+        }
+        //if (counter == post.length)
+        //result = true
+        result = counter == post.length;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/job4j/array/EndsWithTest.java
@@ -0,0 +1,39 @@
+package job4j.array;
+
+import org.junit.Test;
+import ru.job4j.array.EndsWith;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenNotStartWithPrefixThenTrue1() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l','l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenNotStartWithPrefixThenFalse2() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l','l', 'a','o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
