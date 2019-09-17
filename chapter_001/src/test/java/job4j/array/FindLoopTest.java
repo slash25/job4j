ac3ba668ff73a0a4b2381c33e54491918d99d676
slash25 <jamaslash@gmail.com>
2019-09-17 10:22:35 +0300

    FindLoop search by Index with Tests

--- chapter_001/src/test/java/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/job4j/array/FindLoopTest.java
@@ -1,28 +1,29 @@
 package job4j.array;
 import org.junit.Test;
-import ru.job4j.array.FindLoop;
+import ru.job4j.array.FindLoopNew;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
+
     @Test
-    public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
+    public void whenArrayHasLengh5Then0() {
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoopNew.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas0Then0() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
-        int value = 0;
-        int result = find.indexOf(input, value);
-        int expect = -1;
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoopNew.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
