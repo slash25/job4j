3a441bf38370b0bcf72c4f79f71930359779de24
slash25 <jamaslash@gmail.com>
2019-09-29 15:02:38 +0300

    Defragment Array  with Test

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -22,8 +22,6 @@ public class Defragment {
                     newArray[ind] = null;
                 }
             }
-           // System.out.print(array[index] + " ");
-
         return array;
     }
 
