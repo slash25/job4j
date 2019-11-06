2330be38cf1b78cbea9a20a33f9cb52fd784f4ba
slash25 <jamaslash@gmail.com>
2019-11-06 21:52:07 +0200

    1.4. Переводчик Русский-Английский испр.[#169909]

--- chapter_001/src/main/java/ru/job4j/oop/DummyDic.java
+++ chapter_001/src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,12 +2,13 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng){
-       return eng;
+       eng =  "Неизвестное слово " + eng;
+       return  eng;
     }
 
     public static void main(String[] args) {
         DummyDic vokabulary = new DummyDic();
         String say = vokabulary.engToRus("word");
-        System.out.println("Неизвестное слово. " + say);
+        System.out.println(say);
     }
 }
