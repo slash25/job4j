73074c84d73126002a3be0c998a0acb131499b63
slash25 <jamaslash@gmail.com>
2019-12-13 23:51:43 +0200

    Реализация профессий в коде[#169913] исправленый 3

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,14 +1,13 @@
 package ru.job4j.tracker;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
-  class Tracker {
+class Tracker {
     /**
      * Массив для хранение заявок.
      */
-    private final Item[] items = new Item[10];
+    private final Item[] items = new Item[100];
 
     /**
      * Указатель ячейки для новой заявки.
@@ -17,103 +16,126 @@ import java.util.Random;
 
     /**
      * Метод реализаущий добавление заявки в хранилище
+     *
      * @param item новая заявка
      */
-     Item add(Item item) {
+    Item add(Item item) {
         item.setId(this.generateId());
         this.items[this.position++] = item;
         return item;
     }
 
-    /**Метод удаляет заявку найдя её  по нужному id
+    /**
+     * Метод удаляет заявку найдя её  по нужному id
      *
      * @param id input id
      */
-     boolean delete(String id){
-        Item[] newArray = new Item[this.position];//10
-
+    boolean delete(String id) {
         boolean res = false;
-        for (int count = 0; count < this.position; count++) {
+        for (int count = 0; count < items.length; count++)
             if (findById(id) != null) {//если пользователь есть в массиве
-                System.arraycopy(items, count + 1, newArray, 0, this.position);
+                for (Item it : items) {
+                    if (it.getId().equals(id)) {
+                        items[count] = null;
+                        break;
+                    }
+                }
                 //нужно сместить массив на 1 ячейку влево а предудущая ячейка стирается
+                System.arraycopy(items, count+1, items, count, position - count);
+                position--;
                 res = true;
-                break;
+                return res;
             }
-        }
         return res;
     }
 
-    /**Метод находит заявку по id
+    /**Метод вычисляющий количество не пустых ячеек
+     *
+     * @param item value
+     * @return i
+     */
+    int countNotNull(Item[] item) {
+        int i = 0;
+        for (Item it : item) {
+            if (it != null) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * Метод находит заявку по id
      *
      * @param id input id
      * @return item
      */
-     Item findById(String id) {
+    Item findById(String id) {
         for (Item item : this.items) {
-            if (item != null) {
-                if (item.getId().equals(id)) {
-                    return item;
-                }
+            if (item.getId().equals(id)) {
+                return item;
             }
+            break;
         }
         return null;
     }
 
-      /**Метод возвращает элементы у которых совпадает имя
-       *
-       * @param name input name
-       * @return result
-       */
-      Item[] findByName(String name) {
-          Item[] result = new Item[this.position];
-          int j = 0;
-              for (Item item : this.items) {
-                  if (item != null) {
-                      if (item.getName().equals(name)) {
-                          result[j] = item;
-                      }
-                      j++;
-                  }
-              }
-              return result;
-          }
-
-    /**Метод заменяет заявку найдя её по id
+    /**
+     * Метод возвращает элементы у которых совпадает имя
      *
-     * @param id id
-     * @param item item
-     * @return  res
+     * @param name input name
+     * @return result
      */
-    public    boolean replace(String id, Item item){
+    Item[] findByName(String name) {
+        Item[] result = new Item[this.position];
+        int j = 0;
+        for (Item item : this.items) {
+            if (item != null) {
+                if (item.getName().equals(name)) {
+                    result[j] = item;
+                }
+                j++;
+            }
+        }
+        return Arrays.copyOf(result, countNotNull(result));
+    }
+
+    /**
+     * Метод заменяет заявку найдя её по id
+     *
+     * @param id   id
+     * @param item item
+     * @return res
+     */
+    public boolean replace(String id, Item item) {
         int countPos = 0;
         boolean res = false;
-        if (findById(id) != null){
-            res = true;
-            for (Item updItem : this.items) {
-                if (updItem.getId().equals(id)){
-                    item.setId(id);
-                    this.items[countPos] = item;
-                    break;
-                }
-                countPos ++;
+        for (Item updItem : this.items) {
+            if (updItem.getId().equals(id)) {
+                item.setId(id);
+                this.items[countPos] = item;
+                res = true;
+                break;
             }
+            countPos++;
         }
         return res;
     }
 
-    /**Метод выводит все заявки
+    /**
+     * Метод выводит все заявки
      *
      * @return result
      */
-    public    Item[] findAll (){
-        Item[] result = new Item[this.position];
+    public Item[] findAll() {
+        //Item[] result = new Item[this.position];
         return Arrays.copyOf(items, position);
     }
 
     /**
      * Метод генерирует уникальный ключ для заявки.
      * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
      * @return Уникальный ключ.
      */
     private String generateId() {
@@ -121,11 +143,11 @@ import java.util.Random;
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
-      @Override
-      public String toString() {
-          return "Tracker{" +
-                  "items=" + Arrays.toString(items) +
-                  ", position=" + position +
-                  '}';
-      }
-  }
+    @Override
+    public String toString() {
+        return "Tracker{" +
+                "items=" + Arrays.toString(items) +
+                ", position=" + position +
+                '}';
+    }
+}
