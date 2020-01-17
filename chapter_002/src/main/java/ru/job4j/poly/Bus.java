a97a305846ccb1903eb60f8af52a804ae089c23b
slash25 <jamaslash@gmail.com>
2020-01-17 22:33:17 +0200

    Интерфейс Input

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passagers(Integer value) {
+
+    }
+
+    @Override
+    public int fill(int gas) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+    void passagers(Integer value);
+    int fill(int gas);
+}
