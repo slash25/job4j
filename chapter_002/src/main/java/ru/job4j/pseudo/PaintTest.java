74ad75f5444fa9dafc1ed82243c4fe1599ee18b1
slash25 <jamaslash@gmail.com>
2020-03-17 13:43:48 +0200

    4. Используя шаблон проектирования - стратегию

--- chapter_002/src/main/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/PaintTest.java
@@ -1,41 +1,35 @@
 package ru.job4j.pseudo;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
-    // поле содержит дефолтный вывод в консоль.
-    private final PrintStream stdout = System.out;
-    // буфер для результата.
-    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-    @Before
-    public void loadOutput() {
-        System.out.println("execute before method");
-        System.setOut(new PrintStream(this.out));
-    }
-
-    @After
-    public void backOutput() {
-        System.setOut(this.stdout);
-        System.out.println("execute after method");
-    }
-
     @Test
+
     public void whenDrawSquare() {
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфер для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+
         new Paint().draw(new Square());
+
+        //5. Сравниваем значения.
         assertThat(
-                this.out.toString(),
+                new String(out.toByteArray()),
                 is(
                         new StringJoiner(
-                        System.lineSeparator(), "",
-                        System.lineSeparator())
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
                                 .add("++++++++")
                                 .add("+      +")
                                 .add("+      +")
@@ -44,14 +38,24 @@ public class PaintTest {
                                 .toString()
                 )
         );
+        // возвращаем обратно стандартный вывод в консоль.
         System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
+
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфер для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+
         new Paint().draw(new Triangle());
         assertThat(
-                this.out.toString(),
+                new String(out.toByteArray()),
                 is(
                         new StringJoiner(
                                 System.lineSeparator(), "",
@@ -63,6 +67,37 @@ public class PaintTest {
                                 .toString()
                 )
         );
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawRomb() {
+
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфер для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+
+        new Paint().draw(new Romb());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("    +    ")
+                                .add("  +   +  ")
+                                .add("+       +")
+                                .add("  +    + ")
+                                .add("    +    ")
+                                .toString()
+                )
+        );
+        // возвращаем обратно стандартный вывод в консоль.
         System.setOut(stdout);
     }
 }
