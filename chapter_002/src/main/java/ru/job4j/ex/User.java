618044eeeec07a63f2c6361b1ce6e6b7e7631352
slash25 <jamaslash@gmail.com>
2020-03-18 22:41:48 +0200

    0.5. Иерархия исключений и множественный catch (with nulls)

--- chapter_002/src/main/java/ru/job4j/ex/User.java
+++ chapter_002/src/main/java/ru/job4j/ex/User.java
@@ -1,12 +1,14 @@
 package ru.job4j.ex;
 
 public class User {
+
     private String username;
     private boolean valid;
     public User(String username, boolean valid) {
         this.username = username;
         this.valid = valid;
     }
+
     public String getUsername() {
         return username;
     }
--- chapter_002/src/main/java/ru/job4j/ex/UserStore.java
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -1,39 +1,42 @@
 package ru.job4j.ex;
 
 public class UserStore {
+    private int position = 0;
 
     //findUser - если пользователя не нашли в списке, то кинуть исключение UserNotFoundException.
 
     private static User findUser(User[] user, String login) throws UserNotFoundException {
 
         for (User us : user) {
-            if ((us.getUsername().equals(login))) {
-                if (us.getUsername().length() > 3) {
-                    return us;
+            if (us != null) {
+                if ((us.getUsername().equals(login))) {
+                    if (us.getUsername().length() > 3) {
+                        return us;
+                    }
+                    throw new UserInvalidException("small name of user");
                 }
-                throw new UserInvalidException("small name of user");
             }
         }
-        throw new UserNotFoundException("user not found");
-    }
-
-    private static boolean validate(User user) throws UserInvalidException {
-        //validate - если пользователь не валидный или если имя пользователя
-        // состоит из менее трех символов, то кинуть исключение UserInvalidException
-
-        //Метод validate принимает аргумент user. У объекта user есть метод isValid().
-        // Если он false, то нужно выкинуть исключение UserInvalidException.
-
-        //Так же в этом методе нужно проверить, что у объекта user количество символов
-        // в поле username меньше 3. Если оно меньше, то нужно выкинуть исключение UserInvalidException.
-        // Для этого нужно воспользоваться методом String.lenght().
-        if (!user.isValid()) {
-            throw new UserInvalidException("user not valid");
+            throw new UserNotFoundException("user not found");
         }
-        return true;
-    }
 
-    public static void main(String[] args) throws UserNotFoundException {
+        private static boolean validate(User user) throws UserInvalidException {
+            //validate - если пользователь не валидный или если имя пользователя
+            // состоит из менее трех символов, то кинуть исключение UserInvalidException
+
+            //Метод validate принимает аргумент user. У объекта user есть метод isValid().
+            // Если он false, то нужно выкинуть исключение UserInvalidException.
+
+            //Так же в этом методе нужно проверить, что у объекта user количество символов
+            // в поле username меньше 3. Если оно меньше, то нужно выкинуть исключение UserInvalidException.
+            // Для этого нужно воспользоваться методом String.lenght().
+            if (!user.isValid()) {
+                throw new UserInvalidException("user not valid");
+            }
+            return true;
+        }
+
+        public static void main(String[] args) throws UserNotFoundException {
 
         /*main - поправить код за счет использования try-catch с множественным блоком catch.
 
@@ -46,19 +49,19 @@ public class UserStore {
         /*User[] users = {new User("Petr Arsentev", true),
                 new User("Dmitriy Galkin", false),
                 new User("Max Petrov", true)};*/
-        User[] users = new User[10];
-        users[0] = new User("Petr Arsentev", true);
-        users[1] = new User("Dmitriy Galkin", false);
-        users[2] = new User("Dj", true);
+            User[] users = new User[10];
+            users[0] = new User("Petr Arsentev", true);
+            users[1] = new User("Dmitriy Galkin", true);
+            users[2] = new User("Dj", true);
 
-        User user = findUser(users, "Petr Arsentev");
+            User user = findUser(users, "Dmitriy Galkin");
 
-        try {
-            if (validate(user)) {
-                System.out.println("This user has an access");
+            try {
+                if (validate(user)) {
+                    System.out.println("This user has an access");
+                }
+            } catch (UserNotFoundException ea) {
+                ea.printStackTrace();
             }
-        } catch (UserNotFoundException ea) {
-            ea.printStackTrace();
         }
     }
-}
