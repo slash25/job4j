bfe5fcc0af062f500016c2146f0fdb38f854750d
slash25 <jamaslash@gmail.com>
2020-07-24 00:04:33 +0300

    PhoneDictionary3

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionaryTest.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -13,11 +13,28 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Arsentev");
+        assertThat(persons.get(0).getName(), is("Petr"));
+    }
+
     public void whenFindByPhone() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk"));
         ArrayList<Person> persons = phones.find("534872");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Bryansk");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 }
